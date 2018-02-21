a1f4569c0f758e6efc21b5ba2cb358d619448d38
Tyler Hancock <darklime@live.ca>
2018-02-21 16:37:55 -0700

    Updated to 1.11 and added jar signing and new build system.

--- src/main/java/net/darkhax/damagecontrol/DamageControl.java
+++ src/main/java/net/darkhax/damagecontrol/DamageControl.java
@@ -2,22 +2,17 @@ package net.darkhax.damagecontrol;
 
 import net.darkhax.damagecontrol.handler.ConfigurationHandler;
 import net.darkhax.damagecontrol.handler.DamageHandler;
-import net.darkhax.damagecontrol.libs.Constants;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Constants.MODID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER)
+@Mod(modid = "dmgcontrol", name = "Damage Control", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
 public class DamageControl {
-    
-    @Instance(Constants.MODID)
-    public static DamageControl instance;
-    
+
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
-        
+
         MinecraftForge.EVENT_BUS.register(new DamageHandler());
         ConfigurationHandler.initConfig(event.getSuggestedConfigurationFile());
         ConfigurationHandler.initGlobalModifiers();
--- src/main/java/net/darkhax/damagecontrol/handler/DamageHandler.java
+++ src/main/java/net/darkhax/damagecontrol/handler/DamageHandler.java
@@ -4,7 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -12,46 +12,43 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class DamageHandler {
-    
-    public boolean hasWorldLoaded = false;
-    
+
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onWorldLoad (WorldEvent.Load event) {
-        
-        if (hasWorldLoaded)
-            return;
-        
-        for (String entityId : EntityList.ENTITY_EGGS.keySet()) {
-            
+
+        for (final ResourceLocation entityId : EntityList.ENTITY_EGGS.keySet()) {
+
             final Entity entity = EntityList.createEntityByIDFromName(entityId, event.getWorld());
-            
+
             if (entity instanceof EntityLivingBase) {
-                
+
                 ConfigurationHandler.getMaxHealth(entityId, (EntityLivingBase) entity);
             }
         }
     }
-    
+
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onEntityJoinWorld (EntityJoinWorldEvent event) {
-        
+
         if (event.getEntity() instanceof EntityLivingBase) {
-            
+
             final EntityLivingBase entity = (EntityLivingBase) event.getEntity();
-            float currentMaxHealth = (float) entity.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).getAttributeValue();
-            float modifiedMaxHealth = ConfigurationHandler.getMaxHealth(entity) * ConfigurationHandler.healthModifier;
-            
-            if (currentMaxHealth != modifiedMaxHealth)
+            final float currentMaxHealth = (float) entity.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).getAttributeValue();
+            final float modifiedMaxHealth = ConfigurationHandler.getMaxHealth(entity) * ConfigurationHandler.healthModifier;
+
+            if (currentMaxHealth != modifiedMaxHealth) {
                 entity.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(modifiedMaxHealth);
-            
-            if (entity.getHealth() == currentMaxHealth)
+            }
+
+            if (entity.getHealth() == currentMaxHealth) {
                 entity.setHealth(modifiedMaxHealth);
+            }
         }
     }
-    
+
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void onEntityHurt(LivingHurtEvent event) {
-        
+    public void onEntityHurt (LivingHurtEvent event) {
+
         event.setAmount(event.getAmount() * ConfigurationHandler.getDamageModifier(event.getSource()));
     }
 }
